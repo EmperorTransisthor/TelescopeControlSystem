34094e592cdeee34d9c590d89fa6fe513abafdd3
but19pl <48327929+but19pl@users.noreply.github.com>
2021-01-26 00:19:51 +0100

    Update functions.h

--- functions.h
+++ functions.h
@@ -5,8 +5,8 @@
 #include "globals.h"
 
 char* hemisphere(void);					// if ID = 0 returns South, if ID = 1 returns North
-char* mode_Asc(void);														// checks Ascension   axis stepper state
-char* mode_Dec(void);														// checks Declination axis stepper state
+char* mode_Asc(void);					// checks Ascension   axis stepper state
+char* mode_Dec(void);					// checks Declination axis stepper state
 
 void SysTick_Handler(void);
 void readColumns(void);
