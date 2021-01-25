5e455b577cfa906bdb8ab5393beadda66a7619e4
but19pl <48327929+but19pl@users.noreply.github.com>
2021-01-26 00:14:12 +0100

    Update klaw.h

--- klaw.h
+++ klaw.h
@@ -4,13 +4,6 @@
 #include "frdm_bsp.h"
 #include "globals.h"
 
-#define PTA5_8_MASK	0x000001E0
-#define PTA9_12_MASK	0x00001E00
-#define KROK1	0xFFFFFFDF
-#define KROK2	0xFFFFFFBF
-#define KROK3	0xFFFFFF7F
-#define KROK4	0xFFFFFEFF
-
 
 void Klaw_Init(void);
 
